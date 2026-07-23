ffe885bf8b0fc49c60a6a48858137682caf1b75d
Bram <brdv@pm.me>
2026-07-23 16:53:13 +0200

    Update Homebrew formula for v0.1.5

--- Formula/stck.rb
+++ Formula/stck.rb
@@ -1,12 +1,11 @@
 class Stck < Formula
   desc "CLI for stacked GitHub pull request workflows"
   homepage "https://github.com/brdv/stck"
-  version "0.1.4"
-  depends_on arch: :arm64
+  url "https://github.com/brdv/stck/releases/download/v0.1.5/stck-v0.1.5-aarch64-apple-darwin.tar.gz"
+  version "0.1.5"
+  sha256 "15acda6bdbf8e2cba3ab7c2d16cbc78c8723de9989686fcfb2b78fa1973f1375"
 
-  url "https://github.com/brdv/stck/releases/download/v#{version}/stck-v#{version}-aarch64-apple-darwin.tar.gz"
-  # Replace this SHA256 when publishing a new release.
-  sha256 "8b1f430cca145239ccec91ba08de28452ff9199476fe3c0cd41b210e6da32a62"
+  depends_on arch: :arm64
 
   def install
     bin.install "stck"
